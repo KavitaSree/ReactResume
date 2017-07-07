9467b7e0cc1b1385f5101fcb195c61f440cce3da
KavitaSree <kavita.pillai@lic.co.nz>
2017-07-07 15:40:29 +1200

    DEV-1 Added the directory structure and empty components

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Components/Header';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+import Testimonials from './Components/Testimonials';
+import Portfolio from './Components/Portfolio';
+import Footer from './Components/Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <About />
+        <Resume />
+        <Contact />
+        <Testimonials />
+        <Portfolio />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
